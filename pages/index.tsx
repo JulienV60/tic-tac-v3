38a0f8bfae62922d8afb6408a344bbb308848c2e
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-04-19 12:15:53 +0200

    button

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,17 @@ import { Card, Button } from "react-bootstrap";
 const Home: NextPage = (props: any) => {
   return (
     <div className="entireHomepage">
-      {/* ======= HOMEPAGE NO CONNECTED START ======== */}
-
-      <div className="d-flex justify-content-end">
+      <div className="">
         <Button
               type="submit"
-              className="home-btn"
+              className="home-btn-one"
               variant="#2f9dac"
               color="#2f9dac"
             >
               Acces RH
             </Button>
         </div>
+      {/* ======= HOMEPAGE NO CONNECTED START ======== */}
 
       <section className="home-no-connected">
         <div className="info-card">
