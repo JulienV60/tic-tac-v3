1cefbd75d0506aa23f1f8492b69858bf1b21629f
mansuy maxime <mmansuy@norauto.fr>
2022-04-20 09:29:31 +0200

    img user not found

--- pages/manager/index.tsx
+++ pages/manager/index.tsx
@@ -40,7 +40,12 @@ export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
 
     //return prenom id and img of collaborateurs
     const listPrenom = listCollaborateurs.map((element) => {
-      return { prenom: element.prenom, _id: element._id, img: element.img };
+      if (element.img === "") {
+        return { prenom: element.prenom, _id: element._id, img: " https://img.wattpad.com/8f19b412f2223afe4288ed0904120a48b7a38ce1/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f5650722d38464e2d744a515349673d3d2d3234323931353831302e313434336539633161633764383437652e6a7067?s=fit&w=720&h=720" };
+
+      } else {
+        return { prenom: element.prenom, _id: element._id, img: element.img };
+      }
     });
 
     const data = await fetch(
